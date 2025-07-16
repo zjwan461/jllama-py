3629ddbc1a1858bcf66495cc3b40df061fc4ee66
suben <826935261@qq.com>
2025-07-16 14:47:19 +0800

    新增ip adapter支持

--- jllama/ai/sd_ip_adapter_faceid.py
+++ jllama/ai/sd_ip_adapter_faceid.py
@@ -1,5 +1,6 @@
 import cv2
 from insightface.app import FaceAnalysis
+from insightface.utils import face_align
 import torch
 import os
 import matplotlib.pyplot as plt
@@ -15,20 +16,21 @@ image = cv2.imread(r"C:\Users\89712\Pictures\liuyifei.webp")
 faces = app.get(image)
 
 faceid_embeds = torch.from_numpy(faces[0].normed_embedding).unsqueeze(0)
-# print(faceid_embeds)
-
+face_image = face_align.norm_crop(image, landmark=faces[0].kps, image_size=224)  # you can also segment the face
 
 import torch
 from diffusers import StableDiffusionPipeline, DDIMScheduler, AutoencoderKL
 from PIL import Image
 
-from ip_adapter.ip_adapter_faceid import IPAdapterFaceID
+from ip_adapter.ip_adapter_faceid import IPAdapterFaceID, IPAdapterFaceIDPlus
+
 # from ip_adapter.ip_adapter_faceid import IPAdapterFaceIDPlus
 
 
 sd_origin_model_path = r"E:\models\AI-ModelScope\stable-diffusion-v1-5"
 base_model_path = r"E:\models\MusePublic\majicMIX_realistic_maijuxieshi_SD_1_5\majicmixRealistic_v7.safetensors"
 vae_model_path = r"E:/models\zhuzhukeji\sd-vae-ft-mse"
+image_encoder_path = "laion/CLIP-ViT-H-14-laion2B-s32B-b79K"
 ip_ckpt = r"E:\models\guaidao\IP-Adapter-FaceID\ip-adapter-faceid_sd15.bin"
 lora_ckpt = r"E:\models\guaidao\IP-Adapter-FaceID\ip-adapter-faceid_sd15_lora.safetensors"
 
@@ -59,7 +61,8 @@ pipe = StableDiffusionPipeline.from_single_file(
 
 # load ip-adapter
 
-ip_model = IPAdapterFaceID(pipe, ip_ckpt, device)
+# ip_model = IPAdapterFaceID(pipe, ip_ckpt, device)
+ip_model = IPAdapterFaceIDPlus(pipe, image_encoder_path, ip_ckpt, device,)
 
 # generate image
 prompt = "1girl,naked,beach,busty"
@@ -67,7 +70,8 @@ negative_prompt = "monochrome, lowres, bad anatomy, worst quality, low quality, 
 
 seed = randint(1, 100000000)
 images = ip_model.generate(
-    prompt=prompt, negative_prompt=negative_prompt, faceid_embeds=faceid_embeds, num_samples=1, width=512, height=768,
+    prompt=prompt, negative_prompt=negative_prompt, face_image=face_image, faceid_embeds=faceid_embeds, num_samples=1,
+    width=512, height=768,
     num_inference_steps=30, seed=seed, guidance_scale=7.5
 )
 # 显示图像
