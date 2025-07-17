ae0b76a309886ca2f1d3c1929c940c05b57719ec
suben <826935261@qq.com>
2025-07-17 16:26:10 +0800

    update version

--- jllama/update/1.1.10/update.sql
+++ jllama/update/1.1.10/update.sql
@@ -1,17 +1,1 @@
-alter table sd_info
-    add column ip_adapter_state varchar(50) default '待初始化';
-
-alter table sd_info
-    add column ip_adapter_faceid_state varchar(50) default '待初始化';
-
-alter table sd_info
-    add column ip_adapter_model_path varchar(500);
-
-alter table sd_info
-    add column insightface_model_path varchar(500);
-
-alter table sd_info
-    add column image_encoder_model_path varchar(500);
-
-alter table sd_info
-    add column ip_adapter_faceid_model_path varchar(500);
+select 1
